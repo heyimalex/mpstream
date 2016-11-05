01e30e2e1b3b524882064f7f3e0b6c92997c7bc8
Alex Guerra <alex@heyimalex.com>
2016-11-05 02:05:51 -0500

    You crazy man

--- mpstream2.go
+++ mpstream2.go
@@ -46,22 +46,21 @@ func BuildWithBoundary2(boundary string, parts []Part) (*mpstream, int64) {
 	streamer := &mpstream{
 		mode: mBoundary,
 		index: 0,
-		offset: 2,
+		offset: 2, // The head delimiter is conveniently just delimiter[2:]
 		delimiter: []byte("\r\n--" + boundary + "\r\n"),
 		headers: headers,
 		bodies: bodies,
-		tailDelimiter: []byte("\r\n--" + boundary + "--\r\n"),
+		trailer: []byte("\r\n--" + boundary + "--\r\n"),
 	}
 	return streamer, size
 }
-
-// modes: boundary, header, body, end
 
 type readmode int
 const (
 	mBoundary = iota
 	mHeader
 	mBody
+	mTrailer
 )
 
 type mpstream struct {
@@ -74,7 +73,7 @@ type mpstream struct {
 	delimiter      []byte
 	headers        [][]byte
 	bodies         []io.Reader
-	tailDelimiter  []byte
+	trailer        []byte
 
 	// todo: check if size was accurate
 	// todo: aggregate closing errors
@@ -82,63 +81,148 @@ type mpstream struct {
 
 func (s *mpstream) Read(p []byte) (n int, err error) {
 
-	for s.index < len(s.bodies) {
+	var (
+		delimiter []byte   = s.delimiter
+		index     int      = s.index
+		offset    int      = s.offset
+		header    []byte
+		nn        int
+	)
 
-		if s.mode == mBoundary {
-			nn := copy(p[n:], s.delimiter[s.offset:])
-			s.offset += nn
-			n += nn
-			if s.offset == len(s.delimiter) {
-				s.offset = 0
-				s.mode = mHeader
-			}
-			if n == len(p) {
-				return n, nil
-			}
-		}
-
-		if s.mode == mHeader {
-			header := s.headers[s.index]
-			nn := copy(p[n:], header[s.offset:])
-			s.offset += nn
-			n += nn
-			if s.offset == len(header) {
-				s.offset = 0
-				s.mode = mBody
-			}
-			if n == len(p) {
-				return n, nil
-			}
-		}
-
-		if s.mode == mBody {
-			body := s.bodies[s.index]
-			nn, err := body.Read(p[n:])
-			n += nn
-			if err != nil {
-				if err != io.EOF {
-					return n, err
-				}
-				s.index += 1
-				s.mode = mBoundary
-			}
-			if n == len(p) {
-				return n, nil
-			}
-		}
+	switch s.mode {
+	case mBoundary:
+		goto LBOUNDARY
+	case mHeader:
+		goto LHEADER
+	case mBody:
+		goto LBODY
+	case mTrailer:
+		goto LTRAILER
 	}
 
-	nn := copy(p[n:], s.tailDelimiter[s.offset:])
-	s.offset += nn
+LBOUNDARY:
+	nn = copy(p[n:], delimiter[offset:])
+	offset += nn
 	n += nn
-	if s.offset == len(s.tailDelimiter) {
-		return n, io.EOF
+
+	if n == len(p) { // Write complete
+		if offset != len(delimiter) { // Read incomplete
+			s.index = index
+			s.offset = offset
+			s.mode = mBoundary
+		} else { // Read complete
+			s.index = index
+			s.offset = 0
+			s.mode = mHeader
+		}
+		return n, nil
+	} else { // Read complete
+		offset = 0
+		goto LHEADER
 	}
-	//if n == len(p) {
-	return n, nil
-	//}
+
+LHEADER:
+	header = s.headers[index]
+	nn = copy(p[n:], header[offset:])
+	offset += nn
+	n += nn
+
+	if n == len(p) { // Write complete
+		if offset != len(header) { // Read incomplete
+			s.index = index
+			s.offset = offset
+			s.mode = mHeader
+		} else { // Read complete
+			s.index = index
+			s.offset = 0
+			s.mode = mBody
+		}
+		return n, nil
+	} else { // Read complete
+		offset = 0
+		goto LBODY
+	}
+
+LBODY:
+	nn, err = s.bodies[index].Read(p[n:])
+	n += nn
+
+	if err != nil && err != io.EOF {
+		return
+	}
+
+	if n == len(p) {
+		if err == nil {
+			s.index = index
+			s.offset = 0
+			s.mode = mBody
+			return n, nil
+		}
+		index += 1
+		s.index = index
+		s.offset = 0
+		if len(s.bodies) == index {
+			s.mode = mTrailer
+		} else {
+			s.mode = mBoundary
+		}
+		return n, nil
+	} else {
+		index += 1
+		if len(s.bodies) == index {
+			s.mode = mTrailer
+			goto LTRAILER
+		} else {
+			goto LBOUNDARY
+		}
+	}
+
+LTRAILER:
+	nn = copy(p[n:], s.trailer[offset:])
+	offset += nn
+	n += nn
+	s.offset = offset
+	if offset == len(s.trailer) {
+		return n, io.EOF
+	} else {
+		return n, nil
+	}
 }
 
 func (s *mpstream) Close() error {
-	return nil
+	var errs []error
+	for _, body := range s.bodies {
+		if c, ok := body.(io.Closer); ok {
+			if err := c.Close(); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+	switch len(errs) {
+	case 0:
+		return nil
+	case 1:
+		return errs[0]
+	default:
+		return &multiCloseError{errs}
+	}
+}
+
+type multiCloseError struct {
+	errs []error
+}
+
+func (e *multiCloseError) Error() string {
+	errs := e.errs
+	var msg bytes.Buffer
+	fmt.Fprintf(&msg, "mpstream: %d errors while closing parts: ", len(errs))
+	fmt.Fprintf(&msg, "[%d]: %s", 0, errs[0])
+	for i := 1; i < len(errs); i++ {
+		fmt.Fprintf(&msg, ", [%d]: %s", i, errs[i])
+	}
+	return msg.String()
+}
+
+func (e *multiCloseError) WrappedErrors() []error {
+	return e.errs
 }
